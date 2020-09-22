d39b74490bf40db4b4294ac16159b7617e3031cc
m.muharram <msmaioali@gmail.com>
2020-09-22 05:14:52 +0300

    init commit

---
+++ app/src/main/java/com/muharram/geoquiz/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.muharram.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextview : TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton=findViewById(R.id.t_button)
+        falseButton=findViewById(R.id.f_button)
+        nextButton=findViewById(R.id.next_button)
+        questionTextview=findViewById(R.id.q_text)
+
+        trueButton.setOnClickListener{
+            checkAnswer(true)
+        }
+
+        falseButton.setOnClickListener{
+            checkAnswer(false)
+        }
+
+        updteQustion()
+        nextButton.setOnClickListener{
+            currentIndex=(currentIndex+1)%questionBank.size
+            updteQustion()
+
+        }
+        questionTextview.setOnClickListener(){
+            currentIndex=(currentIndex+1)%questionBank.size
+            updteQustion()
+        }
+
+
+    }
+
+    fun updteQustion(){
+        val questionText = questionBank[currentIndex].restTextId
+        questionTextview.setText(questionText)
+    }
+
+    fun checkAnswer(userAnswer:Boolean){
+
+        val correctAnswer=questionBank[currentIndex].answer
+        val messageResId =if(correctAnswer==userAnswer){
+            R.string.correct_toast
+        }
+        else{
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this,messageResId,Toast.LENGTH_LONG).show()
+    }
+}
